74dc62b7fe2d0521a9bdd3bd7bbfe0ad9a689fac
Nandhini S <140828098+nandhini-1402@users.noreply.github.com>
2024-04-22 20:32:29 +0530

    Create Gt.java
    Find the Greatest of the Two Numbers in Java
    Given two integer inputs N1 and N2, the objective is to write a code to Find the Greatest of the Two Numbers in Java. In order to do so we’ll compare the numbers using if-else statements.
     
    Example
    Input : num1 = 12 and num2 = 3
    Output : 12

---
+++ Gt.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Gt
+{
+	public static void main(String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int n1=sc.nextInt();
+	    int n2=sc.nextInt();
+	    
+	    if(n1>n2){
+	        System.out.print(n1+" is the greatest number");
+	    }
+	    else{
+	        System.out.print(n2+" is the greatest number");
+	    }
+		
+	}
+}
